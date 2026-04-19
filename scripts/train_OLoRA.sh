859e97ae094aaf236eba86b292ec8a3ff73a73be
donggcao18 <caovdongg18@gmail.com>
2026-04-19 22:45:15 +0700

    +

--- scripts/train_OLoRA.sh
+++ scripts/train_OLoRA.sh
@@ -22,7 +22,7 @@ deepspeed --master_port "$port" training/main_anamoe.py \
    --num_test 50 \
    --learning_rate 1e-4 \
    --CL_method O-LoRA \
-   --output_dir ./output_models/OLoRA_Qwen2.5-Coder-1.5B \
+   --output_dir ./output_models/OLoRA_Qwen2.5-Coder-1.5B_with_instruction_pool \
    --per_device_train_batch_size 16 \
    --per_device_eval_batch_size 16 \
    --gradient_accumulation_steps 1 \
