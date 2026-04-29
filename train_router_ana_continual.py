d187a3cd106ee362e7c9cd18eaa65e363a21c524
donggcao18 <caovdongg18@gmail.com>
2026-04-29 16:10:25 +0700

    +

--- train_router_ana_continual.py
+++ train_router_ana_continual.py
@@ -40,9 +40,9 @@ from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 import torch.nn.functional as F
 
 feature_layers = 4
-gamma_global = 10000
-router_weights_path = f'./output_models/router_weights_qwencoder7b_gamma{gamma_global}'
-dataset_cache_path = f'./output_models/outputs_router_dataset_cache_qwencoder7b_gamma{gamma_global}'
+gamma_global = 5000
+router_weights_path = f'./output_models/router_weights_qwencoder_gamma{gamma_global}_batch1'
+dataset_cache_path = f'./output_models/outputs_router_dataset_cache_qwencoder_gamma{gamma_global}_batch1'
 paths = [router_weights_path,dataset_cache_path]
     
 for path in paths:
@@ -188,7 +188,7 @@ class NewLlamaForCausalLM(LlamaForCausalLM):
         pass
 
 
-def load_model_and_tokenizer(model_name_or_path='Qwen/Qwen2.5-Coder-7B'):
+def load_model_and_tokenizer(model_name_or_path='Qwen/Qwen2.5-Coder-1.5B'):
     if 'qwen' in model_name_or_path.lower():
         ModelClass = NewQwen2ForCausalLM
     else:
@@ -359,7 +359,7 @@ def train():
             infer_dataset,
             collate_fn=inf_data_collator,
             sampler=infer_sampler,
-            batch_size=16,
+            batch_size=1,
         )
 
         print("***** Start Training *****")
