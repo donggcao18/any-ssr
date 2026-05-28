f91f09f25cdcce7f7081126c5780023f176e5921
Khanh Tran <ankhanhtran02@gmail.com>
2026-05-28 13:57:04 +0700

    +

--- scripts/executable/train_router_executable.sh
+++ scripts/executable/train_router_executable.sh
@@ -7,7 +7,7 @@ GAMMA="${GAMMA:-10000}"
 python train_router_ana_continual.py \
     --model_name_or_path "Qwen/Qwen2.5-Coder-1.5B" \
     --benchmark executable \
-    --router_weights_path "output_models/router_weights_executable_gamma_{$GAMMA}" \
+    --router_weights_path "output_models/router_weights_executable_gamma_${GAMMA}" \
     --dataset_cache_path  "output_models/outputs_router_executable_cache" \
     --max_prompt_len 1024 \
     --max_ans_len    2048 \
@@ -17,7 +17,7 @@ python train_router_ana_continual.py \
 python eval_router_ana.py \
     --model_name_or_path "Qwen/Qwen2.5-Coder-1.5B" \
     --benchmark executable \
-    --router_weights_path "output_models/router_weights_executable_gamma_{$GAMMA}" \
+    --router_weights_path "output_models/router_weights_executable_gamma_${GAMMA}" \
     --dataset_cache_path  "output_models/outputs_router_executable_cache" \
     --max_prompt_len 1024 \
     --max_ans_len    2048 \
